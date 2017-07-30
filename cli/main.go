5be806d7efaf3adcf7dfcc3a28f680f10918b11b
Drew J. Sonne <drew.sonne@gmail.com>
2017-07-30 10:50:08 +0100

    Fixed version retrieval

--- cli/main.go
+++ cli/main.go
@@ -45,14 +45,14 @@ func main() {
 }
 
 func version() string {
-	if tag := os.Getenv("TRAVIS_TAG"); tag == "" {
-		if commit := os.Getenv("TRAVIS_COMMIT"); commit == "" {
+	if tag := os.Getenv("TRAVIS_TAG"); tag != "" {
+		return tag
+	} else {
+		if commit := os.Getenv("TRAVIS_COMMIT"); commit != "" {
 			return commit
 		} else {
 			return GoCDUtilityDefaultVersion
 		}
-	} else {
-		return tag
 	}
 }
 
