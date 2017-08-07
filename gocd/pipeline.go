fb6ab2f11894551189636d6fa1d03db64ff82389
Drew J. Sonne <drew.sonne@beamly.com>
2017-08-07 14:58:05 +0100

    Fixed formatting

--- gocd/pipeline.go
+++ gocd/pipeline.go
@@ -21,7 +21,7 @@ type Pipeline struct {
 
 // Material describes an artifact dependency for a pipeline object.
 type Material struct {
-	Type string `json:"type"`
+	Type       string `json:"type"`
 	Attributes struct {
 		URL             string      `json:"url"`
 		Destination     string      `json:"destination,omitempty"`
@@ -43,35 +43,35 @@ type PipelineHistory struct {
 // PipelineInstance describes a single pipeline run
 type PipelineInstance struct {
 	BuildCause   BuildCause `json:"build_cause"`
-	CanRun       bool `json:"can_run"`
-	Name         string `json:"name"`
-	NaturalOrder int `json:"natural_order"`
-	Comment      string `json:"comment"`
-	Stages       []Stage `json:"stages"`
+	CanRun       bool       `json:"can_run"`
+	Name         string     `json:"name"`
+	NaturalOrder int        `json:"natural_order"`
+	Comment      string     `json:"comment"`
+	Stages       []Stage    `json:"stages"`
 }
 
 type BuildCause struct {
-	Approver          string `json:"approver,omitempty"`
+	Approver          string             `json:"approver,omitempty"`
 	MaterialRevisions []MaterialRevision `json:"material_revisions"`
-	TriggerForced     bool `json:"trigger_forced"`
-	TriggerMessage    string `json:"trigger_message"`
+	TriggerForced     bool               `json:"trigger_forced"`
+	TriggerMessage    string             `json:"trigger_message"`
 }
 
 type MaterialRevision struct {
 	Modifications []Modification `json:"modifications"`
-	Material struct {
+	Material      struct {
 		Description string `json:"description"`
 		Fingerprint string `json:"fingerprint"`
 		Type        string `json:"type"`
-		ID          int `json:"id"`
-	}`json:"material"`
+		ID          int    `json:"id"`
+	} `json:"material"`
 	Changed bool `json:"changed"`
 }
 
 type Modification struct {
 	EmailAddress string `json:"email_address"`
-	ID           int `json:"id"`
-	ModifiedTime int `json:"modified_time"`
+	ID           int    `json:"id"`
+	ModifiedTime int    `json:"modified_time"`
 	UserName     string `json:"user_name"`
 	Comment      string `json:"comment"`
 	Revision     string `json:"revision"`
