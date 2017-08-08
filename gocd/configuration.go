34ceee0409274869c1f3c5a396971cc64ef36932
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-08 07:06:39 +0100

    Added Repositories

--- gocd/configuration.go
+++ gocd/configuration.go
@@ -2,29 +2,61 @@ package gocd
 
 import (
 	"context"
-	"encoding/xml"
 )
 
 // ConfigurationService describes the HAL _link resource for the api response object for a pipelineconfig
 type ConfigurationService service
 
 type ConfigXML struct {
-	XMLName xml.Name     `xml:"cruise"`
-	Server  ConfigServer `xml:"server"`
+	Repositories []ConfigRepository `xml:"repositories>repository"`
+	Server       ConfigServer `xml:"server"`
+}
+
+type ConfigRepository struct {
+	ID   string `xml:"id"`
+	Name string `xml:"name"`
+	PluginConfiguration
 }
 
 type ConfigServer struct {
-	Elastic                   ConfigElastic `xml:"elastic"`
-	ArtifactsDir              string        `xml:"artifactsdir,attr"`
-	SiteUrl                   string        `xml:"siteUrl,attr"`
-	SecureSiteUrl             string        `xml:"secureSiteUrl,attr"`
-	PurgeStart                string        `xml:"purgeStart,attr"`
-	PurgeUpTo                 string        `xml:"purgeUpto,attr"`
-	JobTimeout                int           `xml:"jobTimeout,attr"`
-	AgentAutoRegisterKey      string        `xml:"agentAutoRegisterKey,attr"`
-	WebhookSecret             string        `xml:"webhookSecret,attr"`
-	CommandRepositoryLocation string        `xml:"commandRepositoryLocation,attr"`
-	ServerId                  string        `xml:"serverId,attr"`
+	MailHost                  MailHost `xml:"mailhost"`
+	Security                  ConfigSecurity `xml:"security"`
+	Elastic                   ConfigElastic  `xml:"elastic"`
+	ArtifactsDir              string         `xml:"artifactsdir,attr"`
+	SiteUrl                   string         `xml:"siteUrl,attr"`
+	SecureSiteUrl             string         `xml:"secureSiteUrl,attr"`
+	PurgeStart                string         `xml:"purgeStart,attr"`
+	PurgeUpTo                 string         `xml:"purgeUpto,attr"`
+	JobTimeout                int            `xml:"jobTimeout,attr"`
+	AgentAutoRegisterKey      string         `xml:"agentAutoRegisterKey,attr"`
+	WebhookSecret             string         `xml:"webhookSecret,attr"`
+	CommandRepositoryLocation string         `xml:"commandRepositoryLocation,attr"`
+	ServerId                  string         `xml:"serverId,attr"`
+}
+
+type MailHost struct {
+	Hostname string `xml:"hostname,attr"`
+	Port     int `xml:"port,attr"`
+	TLS      bool `xml:"tls,attr"`
+	From     string `xml:"from,attr"`
+	Admin    string `xml:"admin,attr"`
+}
+
+type ConfigSecurity struct {
+	AuthConfigs []ConfigAuthConfig `xml:"authConfigs>authConfig"`
+	Roles       []ConfigRole `xml:"roles>role"`
+	Admins      []string `xml:"admins>user"`
+}
+
+type ConfigRole struct {
+	Name  string `xml:"name,attr"`
+	Users []string `xml:"users>user"`
+}
+
+type ConfigAuthConfig struct {
+	ID         string `xml:"id,attr"`
+	PluginId   string `xml:"pluginId,attr"`
+	Properties []ConfigProperty `xml:"property"`
 }
 
 type ConfigElastic struct {
