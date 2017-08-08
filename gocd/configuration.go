47e9db467510461ba15ab526f6468c051a406cff
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-08 06:06:44 +0100

    Added basics of configxml

--- gocd/configuration.go
+++ gocd/configuration.go
@@ -7,19 +7,23 @@ import (
 // ConfigurationService describes the HAL _link resource for the api response object for a pipelineconfig
 type ConfigurationService service
 
+type ConfigXML struct {
+
+}
+
 // Get will retrieve all agents, their status, and metadata from the GoCD Server.
 // Get returns a list of pipeline instanves describing the pipeline history.
-func (cd *ConfigurationService) Get(ctx context.Context) (*PipelineStatus, *APIResponse, error) {
+func (cd *ConfigurationService) Get(ctx context.Context) (*ConfigXML, *APIResponse, error) {
 	req, err := cd.client.NewRequest("GET", "admin/config.xml", nil, "")
 	if err != nil {
 		return nil, nil, err
 	}
 
-	ps := PipelineStatus{}
-	resp, err := cd.client.Do(ctx, req, &ps, responseTypeXML)
+	cx := ConfigXML{}
+	resp, err := cd.client.Do(ctx, req, &cx, responseTypeXML)
 	if err != nil {
 		return nil, resp, err
 	}
 
-	return &ps, resp, nil
+	return &cx, resp, nil
 }
