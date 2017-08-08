942e03b61e246eed73073f436a745610699a930d
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-08 06:35:22 +0100

    Added initial config xml structures

--- gocd/configuration.go
+++ gocd/configuration.go
@@ -2,13 +2,45 @@ package gocd
 
 import (
 	"context"
+	"encoding/xml"
 )
 
 // ConfigurationService describes the HAL _link resource for the api response object for a pipelineconfig
 type ConfigurationService service
 
 type ConfigXML struct {
+	XMLName xml.Name `xml:"cruise"`
+	Server  ConfigServer `xml:"server"`
+}
 
+type ConfigServer struct {
+	Elastic                   ConfigElastic `xml:"elastic"`
+	
+	ArtifactsDir              string `xml:"artifactsdir,attr"`
+	SiteUrl                   string `xml:"siteUrl,attr"`
+	SecureSiteUrl             string `xml:"secureSiteUrl,attr"`
+	PurgeStart                string `xml:"purgeStart,attr"`
+	PurgeUpTo                 string `xml:"purgeUpto,attr"`
+	JobTimeout                int `xml:"jobTimeout,attr"`
+	AgentAutoRegisterKey      string `xml:"agentAutoRegisterKey,attr"`
+	webhookSecret             string `xml:"webhookSecret,attr"`
+	CommandRepositoryLocation string `xml:"commandRepositoryLocation,attr"`
+	ServerId                  string `xml:"serverId,attr"`
+}
+
+type ConfigElastic struct {
+	Profiles []ConfigElasticProfile `xml:"profiles>profile"`
+}
+
+type ConfigElasticProfile struct {
+	ID         string `xml:"id,attr"`
+	PluginID   string `xml:"pluginId,attr"`
+	Properties []ConfigProperty `xml:"property"`
+}
+
+type ConfigProperty struct{
+	Key string `xml:"key"`
+	Value string `xml:"value"`
 }
 
 // Get will retrieve all agents, their status, and metadata from the GoCD Server.
