2ff5a3d30b6f0267f7189bc83a8bd964eb3f78cd
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-09 20:35:07 +0100

    Fixed format errors

--- gocd/encryption.go
+++ gocd/encryption.go
@@ -5,21 +5,23 @@ import (
 	"net/url"
 )
 
-// ConfigurationService describes the HAL _link resource for the api response object for a pipelineconfig
+// EncryptionService describes the HAL _link resource for the api response object for a pipelineconfig
 type EncryptionService service
 
+// CipherText sescribes the response from the api with an encrypted value.
 type CipherText struct {
 	EncryptedValue string       `json:"encrypted_value"`
 	Links          EncryptLinks `json:"_links"`
 }
 
-// AgentsLinks describes the HAL _link resource for the api response object for a collection of agent objects.
+// EncryptLinks describes the HAL _link resource for the api response object for a collection of agent objects.
 //go:generate gocd-response-links-generator -type=EncryptLinks
 type EncryptLinks struct {
 	Self *url.URL `json:"self"`
 	Doc  *url.URL `json:"doc"`
 }
 
+// Encrypt takes a plaintext value and returns a cipher text.
 func (e *EncryptionService) Encrypt(ctx context.Context, value string) (*CipherText, *APIResponse, error) {
 	type plaintext struct {
 		Value string `json:"value"`
