08a780bf6028a606187746cd3d64e0587e76d54e
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-15 17:57:53 +0100

    Fixed TestUnmarshallLinkFieldFail testcase

--- gocd/links_test.go
+++ gocd/links_test.go
@@ -8,8 +8,5 @@ import (
 func TestUnmarshallLinkFieldFail(t *testing.T) {
 	d := linkField{}
 	err := unmarshallLinkField(d, "missing-field", nil)
-	if err != nil {
-		t.Error(err)
-	}
 	assert.EqualError(t, err, "'missing-field' was not present in `map[]`")
 }
