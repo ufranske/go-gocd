063922d9de7f3654f2a2f9e3a9b22200d17b7291
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-15 18:55:33 +0100

    Added tests for pipeline groups fitler

--- gocd/pipelinegroups.go
+++ gocd/pipelinegroups.go
@@ -9,18 +9,6 @@ type PipelineGroupsService service
 type PipelineGroup struct {
 	Name      string      `json:"name"`
 	Pipelines []*Pipeline `json:"pipelines"`
-	//Pipelines []struct {
-	//	Name   string `json:"name"`
-	//	Stages []struct {
-	//		Name string `json:"name"`
-	//	} `json:"stages"`
-	//	Materials []struct {
-	//		Description string `json:"description"`
-	//		Fingerprint string `json:"fingerprint"`
-	//		Type        string `json:"type"`
-	//	} `json:"materials"`
-	//	Label string `json:"label"`
-	//} `json:"pipelines"`
 }
 
 // List Pipeline groups
