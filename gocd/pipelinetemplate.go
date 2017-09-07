da20a1f3a2cce78d506b323ccfabeb0fdd106064
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-07 20:36:44 +0100

    Split functions into resource files

--- gocd/pipelinetemplate.go
+++ gocd/pipelinetemplate.go
@@ -62,46 +62,6 @@ type PipelineTemplate struct {
 	Stages   []*Stage                  `json:"stages,omitempty"`
 }
 
-// GetStages from the pipeline template
-func (pt *PipelineTemplate) GetStages() []*Stage {
-	return pt.Stages
-}
-
-// GetStage from the pipeline template
-func (pt *PipelineTemplate) GetStage(stageName string) *Stage {
-	for _, stage := range pt.Stages {
-		if stage.Name == stageName {
-			return stage
-		}
-	}
-	return nil
-}
-
-// GetName of the pipeline template
-func (pt *PipelineTemplate) GetName() string {
-	return pt.Name
-}
-
-// SetStages overwrites any existing stages
-func (pt *PipelineTemplate) SetStages(stages []*Stage) {
-	pt.Stages = stages
-}
-
-// AddStage appends a stage to this pipeline
-func (pt *PipelineTemplate) AddStage(stage *Stage) {
-	pt.Stages = append(pt.Stages, stage)
-}
-
-// RemoveLinks gets the PipelineTemplate ready to be submitted to the GoCD API.
-func (pt *PipelineTemplate) RemoveLinks() {
-	pt.Links = nil
-}
-
-// Pipelines returns a list of Pipelines attached to this PipelineTemplate object.
-func (pt *PipelineTemplate) Pipelines() []*Pipeline {
-	return pt.Embedded.Pipelines
-}
-
 // Get a single PipelineTemplate object in the GoCD API.
 func (pts *PipelineTemplatesService) Get(ctx context.Context, name string) (*PipelineTemplate, *APIResponse, error) {
 	pt := PipelineTemplate{}
--- gocd/stages.go
+++ gocd/stages.go
@@ -1,9 +1,5 @@
 package gocd
 
-import (
-	"encoding/json"
-	"errors"
-)
 
 // StagesService exposes calls for interacting with Stage objects in the GoCD API.
 type StagesService service
@@ -20,37 +16,3 @@ type Stage struct {
 	Jobs                  []*Job    `json:"jobs,omitempty"`
 }
 
-// JSONString returns a string of this stage as a JSON object.
-func (s *Stage) JSONString() (string, error) {
-	err := s.Validate()
-	if err != nil {
-		return "", err
-	}
-	s.Clean()
-	bdy, err := json.MarshalIndent(s, "", "  ")
-	return string(bdy), err
-}
-
-// Validate ensures the attributes attached to this structure are ready for submission to the GoCD API.
-func (s *Stage) Validate() error {
-	if s.Name == "" {
-		return errors.New("`gocd.Stage.Name` is empty")
-	}
-
-	if len(s.Jobs) == 0 {
-		return errors.New("At least one `Job` must be specified")
-	}
-
-	for _, job := range s.Jobs {
-		if err := job.Validate(); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// Clean the approvel step.
-func (s *Stage) Clean() {
-	s.Approval.Clean()
-}
