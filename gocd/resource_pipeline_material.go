9394138452770494160371f748c5c752ddd0c560
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-24 13:37:12 +0100

    Added equivalance of material method

---
+++ gocd/resource_pipeline_material.go
@@ -0,0 +1,32 @@
+package gocd
+
+import (
+	"fmt"
+)
+
+// Equal is true if the two materials are logically equivalent. Not neccesarily literally equal.
+func (m Material) Equal(a *Material) bool {
+	if m.Type != a.Type {
+		return false
+	}
+	switch m.Type {
+	case "git":
+		return m.Attributes.equalGit(&a.Attributes)
+	default:
+		panic(fmt.Errorf("Material comparison not implemented for '%s'", m.Type))
+	}
+	return false
+}
+
+func (a1 MaterialAttributes) equalGit(a2 *MaterialAttributes) bool {
+	if a1.URL == a2.URL {
+		if a1.Branch == a2.Branch {
+			// Check if branches are equal
+			return true
+		} else if a1.Branch == "" && a2.Branch == "master" || a1.Branch == "master" && a2.Branch == "" {
+			// Check if branches are master equal
+			return true
+		}
+	}
+	return false
+}
