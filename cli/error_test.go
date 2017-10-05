320db8bddd6a7952c31bc3fcaa49dec6c5c2cfea
Drew J. Sonne <drew.sonne@gmail.com>
2017-10-05 20:17:07 +0100

    Added error test cases

--- cli/error_test.go
+++ cli/error_test.go
@@ -1,15 +1,19 @@
 package cli
 
 import (
+	"github.com/drewsonne/go-gocd/gocd"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/cli"
+	"net/http"
+	"net/url"
 	"testing"
 )
 
 func TestError(t *testing.T) {
 	t.Run("Basic", testErrorBasic)
 	t.Run("Type", testErrorType)
+	t.Run("UnexpectedError", testErrorUnexpectedError)
 }
 
 func testErrorType(t *testing.T) {
@@ -25,4 +29,33 @@ func testErrorBasic(t *testing.T) {
   "Error": "test-error",
   "Request": "TestReqType"
 }`, err.Error())
+
+	assert.Equal(t, 1, err.ExitCode())
+}
+
+func testErrorUnexpectedError(t *testing.T) {
+	u, e := url.Parse("http://example.com/")
+	if e != nil {
+		t.Error(e)
+	}
+	err := NewCliError("TestReqType", &gocd.APIResponse{
+		HTTP: &http.Response{
+			StatusCode: 201,
+		},
+		Request: &gocd.APIRequest{
+			HTTP: &http.Request{
+				URL: u,
+			},
+		},
+	}, errors.New("test-error"))
+	assert.Equal(t, `{
+  "Error": "An error occurred while retrieving the resource.",
+  "RequestBody": "",
+  "RequestEndpoint": "http://example.com/",
+  "RequestHeader": "null",
+  "ResponseBody": "",
+  "ResponseHeader": "null",
+  "Status": 201
+}`, err.Error())
+
 }
