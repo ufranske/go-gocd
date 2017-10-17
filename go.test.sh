4c12cc3620bbcb7ce83bdd9fed9c4e9c5a236c2c
Drew J. Sonne <drew.sonne@gmail.com>
2017-10-17 19:35:32 +0100

    Remove extra “mode:atomic” from the middle of the coverage file

--- go.test.sh
+++ go.test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.out
+echo "mode: atomic" > coverage.out
 
 for d in $(glide novendor); do
     go test -v -coverprofile=profile.txt -covermode=atomic $d
     if [ -f profile.txt ]; then
-        cat profile.txt >> coverage.out
+        cat profile.txt | tail -n +2 >> coverage.out
         rm profile.txt
     fi
 done
