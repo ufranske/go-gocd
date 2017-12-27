acc3c5509beea2aa02b3efbcb2b176b77742b11c
Drew J. Sonne <drew.sonne@beamly.com>
2017-12-27 10:34:06 +0000

    Fixed weird package dependency

--- gocd/gocd.go
+++ gocd/gocd.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"github.com/pkg/errors"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +14,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"errors"
 )
 
 const (
