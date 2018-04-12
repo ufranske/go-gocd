a5edffca72f84e2609126fe346435d4c1b97aff3
Drew J. Sonne <drew.sonne@gmail.com>
2018-04-12 08:40:47 +0100

    Fix test cases

--- gocd/role_gocd_test.go
+++ gocd/role_gocd_test.go
@@ -17,7 +17,7 @@ func TestRole(t *testing.T) {
 }
 
 func testRoleGoCD(t *testing.T) {
-	mux.HandleFunc("/go/api/admin/security/roles", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/api/admin/security/roles", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, r.Method, "POST", "Unexpected HTTP method")
 		assert.Equal(t, apiV1, r.Header.Get("Accept"))
 
@@ -25,22 +25,23 @@ func testRoleGoCD(t *testing.T) {
 
 		fmt.Fprint(w, string(j))
 	})
+
 	r, _, err := client.Roles.Create(context.Background(),
 		&Role{
 			Name: "my-mock-gocd-role",
 			Type: "gocd",
-			Attributes: RoleAttributesGoCD{
+			Attributes: &RoleAttributesGoCD{
 				Users: []string{"user-one", "user-two"},
 			},
 		},
 	)
 
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	assert.Equal(t, &Role{
 		Name: "my-mock-gocd-role",
 		Type: "gocd",
-		Attributes: RoleAttributesGoCD{
+		Attributes: &RoleAttributesGoCD{
 			Users: []string{"user-one", "user-two"},
 		},
 	}, r)
