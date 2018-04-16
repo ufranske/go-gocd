954484e3390e3f304d648b361a4c71e5ac9455e1
Drew J. Sonne <drew.sonne@gmail.com>
2018-04-16 18:37:36 +0100

    Add missing comments

--- gocd/role.go
+++ gocd/role.go
@@ -4,15 +4,17 @@ import (
 	"context"
 )
 
-// PropertiesService describes Actions which can be performed on agents
+// RoleService describes Actions which can be performed on roles
 type RoleService service
 
+// Role represents a type of agent/actor who can access resources perform operations
 type Role struct {
 	Name       string              `json:"name"`
 	Type       string              `json:"type"`
 	Attributes *RoleAttributesGoCD `json:"attributes"`
 }
 
+// RoleAttributesGoCD are attributes describing a role, in this cae, which users are present in the role.
 type RoleAttributesGoCD struct {
 	Users []string `json:"users"`
 }
