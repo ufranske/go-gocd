ffe1c385d39f37cfb73ac2c8d053401e51c0278a
Drew J. Sonne <drew.sonne@gmail.com>
2018-08-20 20:29:21 +0100

    Added get and update endpoints for SCM tests

--- gocd/scm_test.go
+++ gocd/scm_test.go
@@ -11,6 +11,10 @@ func TestSCM(t *testing.T) {
 		name          string
 		scmCreate     *SCM
 		scmCreateWant *SCM
+		scmGet        string
+		scmGetWant    *SCM
+		scmUpdate     *SCM
+		scmUpdateWant *SCm
 		scmListWant   []*SCM
 	}{
 		{
@@ -37,6 +41,40 @@ func TestSCM(t *testing.T) {
 					{Key: "url", Value: "https://github.com/sample/example.git"},
 				},
 			},
+			scmGet: "mock-name",
+			scmGetWant: &SCM{
+				ID:             "mock-id",
+				Name:           "mock-name",
+				AutoUpdate:     true,
+				PluginMetadata: &SCMMetadata{},
+				Configuration: []*SCMConfiguration{
+					{Key: "username", Value: "admin"},
+					{Key: "password", EncryptedValue: "1f3rrs9uhn63hd"},
+					{Key: "url", Value: "https://github.com/sample/example.git"},
+				},
+			},
+			scmUpdate: &SCM{
+				ID:             "mock-id",
+				Name:           "updated-mock-name",
+				AutoUpdate:     true,
+				PluginMetadata: &SCMMetadata{},
+				Configuration: []*SCMConfiguration{
+					{Key: "username", Value: "admin"},
+					{Key: "password", EncryptedValue: "1f3rrs9uhn63hd"},
+					{Key: "url", Value: "https://github.com/sample/example.git"},
+				},
+			},
+			scmUpdateWant: &SCM{
+				ID:             "mock-id",
+				Name:           "updated-mock-name",
+				AutoUpdate:     true,
+				PluginMetadata: &SCMMetadata{},
+				Configuration: []*SCMConfiguration{
+					{Key: "username", Value: "admin"},
+					{Key: "password", EncryptedValue: "1f3rrs9uhn63hd"},
+					{Key: "url", Value: "https://github.com/sample/example.git"},
+				},
+			},
 			scmListWant: []*SCM{
 				{
 					ID:             "mock-id",
@@ -55,14 +93,30 @@ func TestSCM(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			if runIntegrationTest(t) {
 				ctx := context.Background()
-				scmCreateGot, _, err := intClient.SCM.Create(ctx, test.scmCreate)
-				assert.NoError(t, err)
+				
+				t.Run("create", func(t *testing.T) {
+					scmCreateGot, _, err := intClient.SCM.Create(ctx, test.scmCreate)
+					assert.NoError(t, err)
+					assert.Equal(t, test.scmCreateWant, scmCreateGot)
+				})
 
-				assert.Equal(t, test.scmCreateWant, scmCreateGot)
+				t.Run("get", func(t *testing.T) {
+					scmGetGot, _, err := intClient.SCM.Get(ctx, test.scmGet)
+					assert.NoError(t, err)
+					assert.Equal(t, test.scmGetWant, scmGetGot)
+				})
 
-				scmListGot, _, err := intClient.SCM.List(ctx)
-				assert.NoError(t, err)
-				assert.Equal(t, test.scmListWant, scmListGot)
+				t.Run("update", func(t *testing.T) {
+					scmUpdateGot, _, err := intClient.SCM.Update(ctx, test.scmUpdate.Name, test.scmUpdate)
+					assert.NoError(t, err)
+					assert.Equal(t, test.scmUpdateWant, scmUpdateGot)
+				})
+
+				t.Run("list", func(t *testing.T) {
+					scmListGot, _, err := intClient.SCM.List(ctx)
+					assert.NoError(t, err)
+					assert.Equal(t, test.scmListWant, scmListGot)
+				})
 			}
 		})
 	}
