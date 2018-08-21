552b394f6829c6b5b3b46ccdbeab50846f3678e7
Drew J. Sonne <drew.sonne@gmail.com>
2018-08-21 13:50:12 +0100

    Add missing slash

--- gocd/resource_server_version.go
+++ gocd/resource_server_version.go
@@ -35,7 +35,7 @@ func init() {
 				newServerAPI("16.12.0", apiV2),
 				newServerAPI("17.9.0", apiV3),
 				newServerAPI("18.3.0", apiV4)),
-			"/api/admin/scms": newVersionCollection(
+			"/api/admin/scms/": newVersionCollection(
 				newServerAPI("16.7.0", apiV1)),
 			"/api/admin/scms/:material_name": newVersionCollection(
 				newServerAPI("16.7.0", apiV1)),
